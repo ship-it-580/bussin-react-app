7569c15888677ff78d4207045ca7a6b4b73f6d00
Juuso Laumo <juuso.laumo@edu.riveria.fi>
2025-02-26 13:37:29 +0200

    bro

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Teretulemasta
+          Teretulemasta!
         </p>
         <p>Painalla linkkiä !!</p>
         <a
