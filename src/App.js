6a5734cb538086695baa345803c2a9dd7578a617
Juuso Laumo <juuso.laumo@edu.riveria.fi>
2025-02-26 12:53:08 +0200

    päivitetty versio

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Jes nyt hän se pelittää
+          Teretulemasta
         </p>
-        <p>Lisäys jutska testataan toimiix livenä häh???</p>
+        <p>Painalla linkkiä !!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
